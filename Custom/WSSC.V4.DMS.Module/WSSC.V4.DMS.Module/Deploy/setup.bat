0068e895c3cd8d943b96f9b9019cef4f84682604
Cybermar95 <dalamar95@gmail.com>
2020-03-26 18:07:03 +0300

    Custom module

---
+++ Custom/WSSC.V4.DMS.Module/WSSC.V4.DMS.Module/Deploy/setup.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+@SET WspInstaller="c:\program files\common files\microsoft shared\web server extensions\12\TEMPLATE\LAYOUTS\WSS\WspInstaller.exe"
+IF NOT EXIST %WspInstaller% @SET WspInstaller="c:\program files\common files\microsoft shared\web server extensions\14\TEMPLATE\LAYOUTS\WSS\WspInstaller.exe"
+IF NOT EXIST %WspInstaller% @SET WspInstaller="..\WspInstaller.exe"
+IF NOT EXIST %WspInstaller% @SET WspInstaller="WspInstaller.exe"
+IF EXIST %WspInstaller% (%WspInstaller% -setup -single -recycle) ELSE (ECHO. & ECHO Could not find WspInstaller.exe. Copy WspInstaller.exe in the current executable folder, or in the folder level above, or in the folder LAYOUTS\WSS)
+PAUSE
---
+++ Custom/WSSC.V4.DMS.Module/WSSC.V4.DMS.Module/Properties/VersionProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WSSC.V4.DMS.Module
+{
+    /// <summary>
+    /// Сведения о версии модуля.
+    /// </summary>
+    public class VersionProvider
+    {
+        /// <summary>
+        /// Номер версии.
+        /// </summary>
+        public const string VersionNumber = "4.0.0.0";
+
+        /// <summary>
+        /// Полное название сборки.
+        /// </summary>
+        public const string AssemblyName = "WSSC.V4.DMS.Module,Version=4.0.0.0,Culture=neutral,PublicKeyToken=9f4da00116c38ec5";
+
+        /// <summary>
+        /// Название модуля.
+        /// </summary>
+        public const string ModuleName = "WSSC.V4.DMS.Module";
+
+        /// <summary>
+        /// Относительный путь к модулю.
+        /// </summary>
+        public const string ModulePath = "/_layouts/WSS/WSSC.V4.DMS.Module";
+    }
+}
