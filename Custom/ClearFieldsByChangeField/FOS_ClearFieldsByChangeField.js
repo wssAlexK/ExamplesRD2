125dcf227f1787cd92c1e07ecfa5660078def656
WSSMZahar <37018330+WSSMZahar@users.noreply.github.com>
2020-03-30 10:58:01 +0500

    З-2020-10390: Перенос функционала, согласно условию задачи

---
+++ Custom/ClearFieldsByChangeField/FOS_ClearFieldsByChangeField.js
@@ -0,0 +1,57 @@
+﻿
+//Проверка полей
+function FOS_ClearFieldsByChangeField_Init() {
+
+    //Проверка поля
+    var mainField = ListForm.GetField(FOS_ClearFieldsByChangeField_JSObject.MainField);
+    if (mainField == null)
+        throw new Error('Не найдено поле ' + FOS_ClearFieldsByChangeField_JSObject.MainField);
+
+    //не поддерживаемые поля
+    var notSupportedFields = ['MSLField', 'DBFieldMarkup', 'PDField', 'CMField', 'PField', 'DBFieldFiles', 'DBFieldFileLink', 'DBFieldLink', 'WSSC.V4.DMS.Fields.TableItems.TableItemsField'];
+
+    //Проверка полей
+    var childs = FOS_ClearFieldsByChangeField_JSObject.ChildFields;
+    for (var i = 0; i < childs.length; i++) {
+        var child = ListForm.GetField(childs[i]);
+
+        if (mainField == null )
+            throw new Error('Не найдено поле ' + childs[i]);
+
+        if (notSupportedFields.indexOf(child.Type) != -1)
+            throw new Error('Тип поля ' + child.Type + ' не поддерживается');
+    }   
+}
+
+//обработчик полей
+function FOS_ClearFieldsByChangeField_Handler() {
+    var childs = FOS_ClearFieldsByChangeField_JSObject.ChildFields;
+    for (var i = 0; i < childs.length; i++) {
+
+        var child = ListForm.GetField(childs[i]);
+        var value = child.GetValue();
+
+        switch (child.Type) {
+            
+            //поля подстановки
+            case 'DBFieldLookupSingle':
+                if (value != "" && value != null)
+                    child.SetValue();
+                break;
+
+            case 'DBFieldLookupMulti':
+                if (value.length > 0)
+                    child.SetValue();
+                break;
+
+            //остальные поля
+            default:
+                if (value != "" && value != null)
+                    child.SetValue();
+                break;
+        }       
+
+
+    }
+}
+
