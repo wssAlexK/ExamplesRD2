56a105cc9a33fbef01d0964c3c2eac79e1ef48c2
akisil <akisil@EKB-DEV-LOCAL19>
2020-04-06 18:36:19 +0500

    Обработчик на проставления подразделения в зависимости от компании пользовател

---
+++ Custom/Consts.cs
@@ -0,0 +1,50 @@
+using System;
+
+public static class Consts
+{
+    public static class Handlers
+    {
+        public static class DepartamentsOnStageHandler
+        {
+            /// <summary>
+            /// [Не актуален]
+            /// </summary>
+            public const string FieldNameDepartamentActual = "Не актуален";
+
+            /// <summary>
+            /// Для какого этапа обработчик
+            /// </summary>
+            public const string StagePreparation = "Подготовка";
+
+            /// <summary>
+            /// Для какого этапа обработчик
+            /// </summary>
+            public const string StageAdd = "Добавление подписанных документов";
+
+            /// <summary>
+            /// [Этап]
+            /// </summary>
+            public const string FieldNameStage = "Этап";
+
+            /// <summary>
+            /// [Подразделение]
+            /// </summary>
+            public const string FieldNameDepartament = "Подразделение";
+
+            /// <summary>
+            /// [Подразделения по совместительству]
+            /// </summary>
+            public const string FieldNameDepartamentExtra = "Подразделения по совместительству";
+
+            /// <summary>
+            /// [Компания]
+            /// </summary>
+            public const string FieldNameCompany = "Компания";
+
+            /// <summary>
+            /// [Инициатор]
+            /// </summary>
+            public const string FieldNameInitor = "Инициатор";
+        }
+    }
+}
